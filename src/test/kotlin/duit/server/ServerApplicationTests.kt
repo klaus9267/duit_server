a44184537c86c107fb27b2381755e711a3b18f9d
klaus9267 <klaus9267@gmail.com>
2025-10-06 05:50:33 +0900

    fix: 테스트 오류 해결

--- src/test/kotlin/duit/server/ServerApplicationTests.kt
+++ src/test/kotlin/duit/server/ServerApplicationTests.kt
@@ -1,12 +1,10 @@
 package duit.server
 
-import duit.server.config.TestJooqConfig
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.context.annotation.Import
 
 @SpringBootTest
-@Import(TestJooqConfig::class)
 class ServerApplicationTests {
 
 	@Test
