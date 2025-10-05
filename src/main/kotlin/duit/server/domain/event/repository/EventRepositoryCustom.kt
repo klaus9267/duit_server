71761ca6df0926ddc47ef7c92b4df831053fd1db
klaus9267 <klaus9267@gmail.com>
2025-10-06 02:49:46 +0900

    fix: jooq 오류 해결

--- src/main/kotlin/duit/server/domain/event/repository/EventRepositoryCustom.kt
+++ src/main/kotlin/duit/server/domain/event/repository/EventRepositoryCustom.kt
@@ -3,9 +3,13 @@ package duit.server.domain.event.repository
 import duit.server.domain.event.dto.EventSearchFilter
 import duit.server.domain.event.entity.Event
 import duit.server.domain.event.entity.EventDate
-import jooq.Tables
+import duit.server.domain.event.entity.EventType
+import duit.server.domain.host.entity.Host
+import jooq.Tables.EVENTS
+import jooq.Tables.HOSTS
 import org.jooq.Condition
 import org.jooq.DSLContext
+import org.jooq.Record
 import org.springframework.data.domain.Page
 import org.springframework.data.domain.PageImpl
 import org.springframework.data.domain.Pageable
@@ -30,13 +34,54 @@ class EventRepositoryCustom(
         val dateCondition = buildDateCondition(eventDate)
 
         return dsl
-            .select()
-            .from(Tables.EVENTS)
-            .join(Tables.HOSTS).on(Tables.HOSTS.ID.eq(Tables.EVENTS.HOST_ID))
-            .where(Tables.EVENTS.IS_APPROVED.eq(true))
+            .select(
+                EVENTS.ID,
+                EVENTS.TITLE,
+                EVENTS.START_AT,
+                EVENTS.END_AT,
+                EVENTS.RECRUITMENT_START_AT,
+                EVENTS.RECRUITMENT_END_AT,
+                EVENTS.URI,
+                EVENTS.THUMBNAIL,
+                EVENTS.IS_APPROVED,
+                EVENTS.EVENT_TYPE,
+                EVENTS.CREATED_AT,
+                EVENTS.UPDATED_AT,
+                HOSTS.ID,
+                HOSTS.NAME,
+                HOSTS.THUMBNAIL,
+            )
+            .from(EVENTS)
+            .join(HOSTS).on(HOSTS.ID.eq(EVENTS.HOST_ID))
+            .where(EVENTS.IS_APPROVED.eq(true))
             .and(dateCondition)
             .orderBy(eventDate.toField())
-            .fetchInto(Event::class.java)
+            .fetch()
+            .map { mapToEvent(it) }
+    }
+
+    private fun mapToEvent(record: Record): Event {
+        val host = Host(
+            id = record.get(HOSTS.ID),
+            name = record.get(HOSTS.NAME),
+            thumbnail = record.get(HOSTS.THUMBNAIL)
+        )
+
+        return Event(
+            id = record.get(EVENTS.ID),
+            title = record.get(EVENTS.TITLE),
+            startAt = record.get(EVENTS.START_AT),
+            endAt = record.get(EVENTS.END_AT),
+            recruitmentStartAt = record.get(EVENTS.RECRUITMENT_START_AT),
+            recruitmentEndAt = record.get(EVENTS.RECRUITMENT_END_AT),
+            uri = record.get(EVENTS.URI),
+            thumbnail = record.get(EVENTS.THUMBNAIL),
+            isApproved = record.get(EVENTS.IS_APPROVED),
+            eventType = EventType.valueOf(record.get(EVENTS.EVENT_TYPE).name),
+            createdAt = record.get(EVENTS.CREATED_AT),
+            updatedAt = record.get(EVENTS.UPDATED_AT),
+            host = host
+        )
     }
 
     private fun buildDateCondition(dateField: EventDate): Condition {
@@ -45,21 +90,21 @@ class EventRepositoryCustom(
 
         return when (dateField) {
             EventDate.START_AT -> {
-                Tables.EVENTS.START_AT.ge(tomorrow)
-                    .and(Tables.EVENTS.START_AT.lt(nextDay))
-                    .and(Tables.EVENTS.START_AT.isNotNull)
+                EVENTS.START_AT.ge(tomorrow)
+                    .and(EVENTS.START_AT.lt(nextDay))
+                    .and(EVENTS.START_AT.isNotNull)
             }
 
             EventDate.RECRUITMENT_START_AT -> {
-                Tables.EVENTS.RECRUITMENT_START_AT.ge(tomorrow)
-                    .and(Tables.EVENTS.RECRUITMENT_START_AT.lt(nextDay))
-                    .and(Tables.EVENTS.RECRUITMENT_START_AT.isNotNull)
+                EVENTS.RECRUITMENT_START_AT.ge(tomorrow)
+                    .and(EVENTS.RECRUITMENT_START_AT.lt(nextDay))
+                    .and(EVENTS.RECRUITMENT_START_AT.isNotNull)
             }
 
             EventDate.RECRUITMENT_END_AT -> {
-                Tables.EVENTS.RECRUITMENT_END_AT.ge(tomorrow)
-                    .and(Tables.EVENTS.RECRUITMENT_END_AT.lt(nextDay))
-                    .and(Tables.EVENTS.RECRUITMENT_END_AT.isNotNull)
+                EVENTS.RECRUITMENT_END_AT.ge(tomorrow)
+                    .and(EVENTS.RECRUITMENT_END_AT.lt(nextDay))
+                    .and(EVENTS.RECRUITMENT_END_AT.isNotNull)
             }
         }
     }
