92e883e232cd3e1f86002d58c4695f613f516da4
klaus9267 <klaus9267@gmail.com>
2025-11-24 04:53:52 +0900

    feat: 조회순 인덱스 추가

--- src/main/kotlin/duit/server/domain/view/entity/View.kt
+++ src/main/kotlin/duit/server/domain/view/entity/View.kt
@@ -4,7 +4,9 @@ import duit.server.domain.event.entity.Event
 import jakarta.persistence.*
 
 @Entity
-@Table(name = "views")
+@Table(name = "views", indexes = [
+    Index(name = "idx_count_event",columnList = "count, event_id")
+])
 class View(
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
